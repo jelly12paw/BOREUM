52e388c7cad60dd9c9244ee73b3b797499393c02
jelly12paw <jelly12paw@gmail.com>
2023-09-21 01:14:29 +0900

    style: Main Component

--- boreum/src/components/Main/Main.tsx
+++ boreum/src/components/Main/Main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styles from './Main.module.css';
+import yellowmoon from '../../assets/yellowmoon.png';
+import whitemoon from '../../assets/whitemoon.png';
 import { useDarkmode } from '../../context/DarkmodeContext';
+import { PiStampLight, PiNotePencilThin } from 'react-icons/pi';
 
 export const Main = () => {
     const { darkmode, toggleDarkmode } = useDarkmode();
-    return  <div>
-                <div>
-                    <button onClick={toggleDarkmode}>
-                        {darkmode ? 'dark' : 'light'}
-                    </button>
+    return  <div className={styles.main}>
+                <div className={styles.container}>
+                    <div className={styles.title}>B<span><img src={darkmode ? yellowmoon : whitemoon} alt={'moon'} /></span>RUEM</div>
+                    <div className={styles.btns}>
+                        <p><PiStampLight/> 도장 찍기</p>
+                        <p><PiNotePencilThin/> 하루 기록</p>
+                    </div>
                 </div>
+                <button onClick={toggleDarkmode}>
+                    Mode
+                    <img src={darkmode ? whitemoon : yellowmoon} alt={'moon'} />
+                </button>
             </div>;
 };
